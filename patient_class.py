375e44c474d08f85b27be89256db04f85339b5a2
lostwiz <1837691@cegepoutaouais.qc.ca>
2022-03-31 11:49:31 -0400

    yohohoh

--- patient_class.py
+++ patient_class.py
@@ -1,2 +1,44 @@
 class patient:
-    def __init__(self,pNoPatient="",pNom="",pPrenom="",pDatNaiss="",pCourriel="",pNbVisites=0,pCommentaire):
+
+    def __init__(self, pNoPatient="", pNom="", pPrenom="", pDateNaiss="",
+                 pCourriel="", pNbVisites=0, pCommentaire=""):
+        self.__noPatient = pNoPatient
+        self.__Nom = pNom
+        self.__Prenom = pPrenom
+        self.__dateNaiss = pDateNaiss
+        self.__courriel = pCourriel
+        self.__nbVisites = pNbVisites
+        self.commentaire = pCommentaire
+
+    def __get__noPatient(self):
+        return self.__noPatient
+
+    def __set__noPatient(self,pNumero):
+        if pNumero.isnumeric() and pNumero.len == 7 :
+            self.__noPatient = pNumero
+
+    noPatient = property(__get__noPatient,__set__noPatient)
+
+    def __get__Nom(self):
+        return self.__Nom
+
+    def __set__nom(self,pnom):
+        if pnom.isalpha() and pnom <= 30:
+            self.__Nom = pnom
+
+    nom = property(__get__Nom,__set__nom)
+
+    def __get__prenom(self):
+        return self.__Prenom
+
+    def __set__prenom(self,pprenom):
+        if pprenom.isalpha() and pprenom <= 30:
+            self.__Prenom = pprenom
+
+    prenom = property(__get__prenom,__set__prenom)
+
+
+
+
+
+
