cb3d44c86b4013a1349b4171134901ab8dab5870
lostwiz <1837691@cegepoutaouais.qc.ca>
2022-04-01 11:20:15 -0400

    hello there general kenobi

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ import patient_class
 import sys
 import code_interface_genere as p
 from PyQt5 import QtWidgets, QtCore
+
 class Fenetreprin(QtWidgets.QMainWindow, p.Ui_MainWindow):
     def __init__(self, parent=None):
         super(Fenetreprin, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle("Gestion des patients")
+    def lbl_hidden(self):
+        self.lbl_erreurnum.hide()
+        self.lbl_erreur_prenom()
+        self.lbl_erreur_nom
+        self.
     @QtCore.pyqtSlot()
     def on_btn_valider_clicked(self):
         nom = self.lnedt_nom.text()
